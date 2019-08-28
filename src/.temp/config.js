2c88f25fd25982dd95997b1fd04028b5cad731f3
noloop <noloop@zoho.com>
2019-08-28 08:36:01 -0300

    Remove florestry.

--- src/.temp/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "siteUrl": "",
-  "siteName": "Brevifolia",
-  "titleTemplate": "%s - Brevifolia",
-  "siteDescription": "",
-  "version": "0.6.8"
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1566991395109
